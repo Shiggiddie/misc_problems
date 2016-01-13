e4f4bbd7c477da8d9f15fdfc288787b474cf9139
Sean Dennison <sean.dennison@twintechs.com>
2016-01-13 13:26:07 -0600

    hole implementation

--- moire_patterns/moire.js
+++ moire_patterns/moire.js
@@ -1,21 +1,47 @@
-var numTriangles = 50;
-var triangle = '<div class="arrow-up"></div';
-var rotator = $('<div class="rotator"></div>');
-var bg = $('<div class="bg"></div>');
+(function() {
+  var w = 500; //TODO base off of window.innerWidth or user input
+  var h = 500; //TODO base off of window.innerHeight or user input
+  var triangleW = 10; //TODO figure out how this is calculated
+  var triangleH = Math.sqrt(3.0)*triangleW/2.0;  
+  var d = w < h ? w : h; //TODO may not be necessary if page bounds are not of concern
+  var numTriangles = Math.floor(d/triangleW);
+  var numRows = Math.floor(d/triangleH);
+  var triangle = '<div class="triangle"></div';
+  var rotator = $('<div class="rotator"></div>');
+  var bg = $('<div class="bg"></div>');
 
-for (var i = 0; i < numTriangles; i++) {
-	var rRow = $('<div class="row"></div>');
-	var bRow = $('<div class="row"></div>');
+  //rotator.css('width', w);
+  //rotator.css('height', w);
 
-	for (var j = 0; j < numTriangles; j++) {
-		rRow.append(triangle);
-		bRow.append(triangle);
-	}
-  rotator.append(rRow);
-  bg.append(bRow);
-	console.log('time through');
-	console.log(rotator);
-}
+  for (var i = 0; i < numRows; i++) {
+    var rRow = $('<div class="row"></div>');
+    var bRow = $('<div class="row"></div>');
 
-$('body').append(rotator);
-$('body').append(bg);
+    for (var j = 0; j < numTriangles; j++) {
+      var t1 = $(triangle);
+      var t2 = $(triangle);
+
+			// TODO add UI for turning colors on/off
+      var a = ((j%3)+(i%3))%3;
+      if (a==0) {
+        t1.css('border-bottom-color', 'red');
+        t2.css('border-bottom-color', 'red');
+      } else if (a==1) {
+        t1.css('border-bottom-color', 'blue');
+        t2.css('border-bottom-color', 'blue');
+      } else {
+        t1.css('border-bottom-color', 'green');
+        t2.css('border-bottom-color', 'green');
+      }
+      rRow.append(t1);
+      bRow.append(t2);
+    }
+    rotator.append(rRow);
+    bg.append(bRow);
+  }
+
+	//TODO add UI for rotation controls
+
+  $('body').append(rotator);
+  $('body').append(bg);
+})()
